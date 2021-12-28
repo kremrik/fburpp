86ca04e91c3b51a3310991b3f7383713c56e2dc5
kremrik <kyle.robert.e@gmail.com>
2021-12-28 10:21:09 -0600

    added select to dummy pipeline

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,6 +6,7 @@ use fburpp::csv::{
     row_to_record,
     CsvRows,
 };
+use fburpp::data::{select};
 
 use std::error::Error;
 
@@ -46,12 +47,18 @@ fn main() -> Result<(), Box<dyn Error>> {
         String::from("str")
     ];
 
+    let select_cols = vec![
+        String::from("foo"),
+        String::from("baz"),
+    ];
+
     let mut reader = make_reader(&input_path);
     let mut writer = make_writer(&output_path);
     let csvrows = CsvRows::new(&mut reader, &col_names, &col_types);
 
     for row in csvrows {
-        let record = row_to_record(row);
+        let sel_row = select(row, &select_cols);
+        let record = row_to_record(sel_row);
         writer.write_record(record)?;
     }
 
